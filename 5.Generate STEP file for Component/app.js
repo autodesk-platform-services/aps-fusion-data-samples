9b637c469813ec504bced99881dc811fe6f3eb88
Adam Nagy <adam_e_nagy@hotmail.com>
2024-02-16 12:18:47 +0000

    fix sample

--- 5.Generate STEP file for Component/app.js
+++ 5.Generate STEP file for Component/app.js
@@ -8,7 +8,7 @@ import path from "path";
 // Application constructor 
 export default class App {
   constructor(accessToken) {
-    this.graphAPI = 'https://developer.api.autodesk.com/graphql';
+    this.graphAPI = 'https://developer.api.autodesk.com/beta/graphql';
     this.accessToken = accessToken;
   }
 
@@ -47,11 +47,11 @@ export default class App {
     if (projects.length < 1)
       throw { message: `Project "${projectName}" does not exist` }
 
-    let files = projects[0].folders.results[0].items.results;
+    let files = projects[0].items.results;
     if (files.length < 1)
       throw { message: `Component "${componentName}" does not exist` }
 
-    return files[0].tipRootComponent.derivatives[0];
+    return files[0].tipRootComponentVersion.derivatives[0];
   }
 
 // <downloadGeometry>
@@ -65,21 +65,17 @@ export default class App {
                 results {
                   projects(filter:{name:$projectName}) {
                     results {
-                      folders {
+                      items(filter:{name:$componentName}) {
                         results {
-                          items(filter:{name:$componentName}) {
-                            results {
-                              ... on MFGDesignItem {
-                                tipRootComponent {
-                                  derivatives (derivativeInput: {outputFormat: STEP, generate: true}) {
-                                    expires
-                                    signedUrl
-                                    status
-                                    progress
-                                    outputFormat
-                                  }       
-                                }
-                              }
+                          ... on MFGDesignItem {
+                            tipRootComponentVersion {
+                              derivatives (derivativeInput: {outputFormat: STEP, generate: true}) {
+                                expires
+                                signedUrl
+                                status
+                                progress
+                                outputFormat
+                              }       
                             }
                           }
                         }
