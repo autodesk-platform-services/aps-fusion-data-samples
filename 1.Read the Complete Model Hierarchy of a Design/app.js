6ae7bceaf667a63b169ff398f0134a1ae78e15ef
Adam Nagy <adam_e_nagy@hotmail.com>
2024-02-16 12:09:16 +0000

    only show main way off getting full hierarchy

--- 1.Read the Complete Model Hierarchy of a Design/app.js
+++ 1.Read the Complete Model Hierarchy of a Design/app.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // Application constructor 
 export default class App {
   constructor(accessToken) {
-    this.graphAPI = 'https://developer.api.autodesk.com/graphql';
+    this.graphAPI = 'https://developer.api.autodesk.com/beta/graphql';
     this.accessToken = accessToken;
   }
 
@@ -34,131 +34,8 @@ export default class App {
     return response;
   }
 
-  getComponentVersion(response, hubName, projectName, componentName) {
-    let hubs = response.data.data.hubs.results;
-    if (hubs.length < 1)
-      throw { message: `Hub "${hubName}" does not exist` }
-      
-    let projects = hubs[0].projects.results;
-    if (projects.length < 1)
-      throw { message: `Project "${projectName}" does not exist` }
-
-    let files = projects[0].rootFolder.items.results;
-    if (files.length < 1)
-      throw { message: `Component "${componentName}" does not exist` }
-
-    return files[0].tipVersion;
-  }
-
-// <getModelHierarchy>
+  // <getModelHierarchy>
   async getModelHierarchy(hubName, projectName, componentName) {
-    try {
-      let response = await this.sendQuery(
-        `query GetModelHierarchy($hubName: String!, $projectName: String!, $componentName: String!) {
-          hubs(filter:{name:$hubName}) {
-            results {
-              name
-              projects(filter:{name:$projectName}) {
-                results {
-                  name
-                  rootFolder {
-                    items(filter:{name:$componentName}) {
-                      results {
-                        ... on Component {
-                          name
-                          tipVersion {
-                            id
-                            name 
-                            modelOccurrences {
-                              results {
-                                componentVersion {
-                                  id
-                                  name
-                                }
-                              }
-                            }
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }`,
-        {
-          hubName,
-          projectName,
-          componentName
-        }
-      )
-
-      let rootComponentVersion = this.getComponentVersion(
-        response, hubName, projectName, componentName
-      );
-      let componentVersions = {};
-      componentVersions[rootComponentVersion.id] = rootComponentVersion;
-
-      await this.getSubComponents(componentVersions, rootComponentVersion.modelOccurrences.results);
-
-      return {
-        rootId: rootComponentVersion.id,
-        componentVersions: componentVersions
-      };
-    } catch (err) {
-      console.log("There was an issue: " + err.message)
-    }
-  }
-// </getModelHierarchy>  
-
-  async getSubComponents(componentVersions, modelReferences) {
-    for (let occurrence of modelReferences) {
-      componentVersions[occurrence.componentVersion.id] ||= {};
-    }
-
-    let query = "query {";
-    let counter = 0;
-    for (let componentVersionId in componentVersions) {
-      // Get info about components we only have the id's of
-      // but no information yet about the component (e.g. its name)
-      if (!componentVersions[componentVersionId]?.name) {
-        query += `
-        _${counter++}: componentVersion(componentVersionId: "${componentVersionId}") {
-          id
-          name
-          modelOccurrences {
-            results {
-              componentVersion {
-                id
-                name
-              }
-            }
-          }
-        }
-      `
-      }
-    }
-    query += "}"
-
-    if (counter === 0) {
-      return;
-    }
-
-    let response = await this.sendQuery(query);
-    for (let componentVersion of Object.values(response.data.data)) {
-      componentVersions[componentVersion.id] = componentVersion;
-    }
-
-    for (let componentVersion of Object.values(response.data.data)) {
-      if (componentVersion.modelOccurrences.results.length > 0) {
-        await this.getSubComponents(componentVersions, componentVersion.modelOccurrences.results);
-      }
-    }
-  }
-
-  // <getModelHierarchyBeta>
-  async getModelHierarchyBeta(hubName, projectName, componentName) {
     try {
       // Get first batch of occurrences
       let response = await this.sendQuery(
@@ -170,29 +47,25 @@ export default class App {
                 projects(filter:{name:$projectName}) {
                   results {
                     name
-                    folders {
+                    items(filter:{name:$componentName}) {
                       results {
-                        items(filter:{name:$componentName}) {
-                          results {
-                            ... on MFGDesignItem {
-                              name
-                              tipRootComponent {
-                                id
-                                name 
-                                allModelOccurrences {
-                                  results {
-                                    parentComponentVersion {
-                                      id 
-                                    }
-                                    componentVersion {
-                                      id
-                                      name
-                                    }
-                                  }
-                                  pagination {
-                                    cursor
-                                  }
+                        ... on MFGDesignItem {
+                          name
+                          tipRootComponentVersion {
+                            id
+                            name 
+                            allModelOccurrences {
+                              results {
+                                parentComponentVersion {
+                                  id 
                                 }
+                                componentVersion {
+                                  id
+                                  name
+                                }
+                              }
+                              pagination {
+                                cursor
                               }
                             }
                           }
@@ -212,7 +85,7 @@ export default class App {
         }
       )
 
-      let rootComponentVersion = response.data.data.nav.hubs.results[0].projects.results[0].folders.results[0].items.results[0].tipRootComponent;
+      let rootComponentVersion = response.data.data.nav.hubs.results[0].projects.results[0].items.results[0].tipRootComponentVersion;
       let cursor = rootComponentVersion.allModelOccurrences.pagination.cursor;
 
       // Keep getting the rest of the occurrences if needed
@@ -255,7 +128,7 @@ export default class App {
       console.log("There was an issue: " + err.message)
     }
   }
-// </getModelHierarchyBeta>  
+// </getModelHierarchy>  
 }
 
 
--- 1.Read the Complete Model Hierarchy of a Design/index.js
+++ 1.Read the Complete Model Hierarchy of a Design/index.js
@@ -8,7 +8,6 @@ const clientSecret = '<YOUR_CLIENT_SECRET>';
 const hubName = '<YOUR_HUB_NAME>';
 const projectName = '<YOUR_PROJECT_NAME>';
 const componentName = '<YOUR_COMPONENT_NAME>';
-const useBetaAPI = true;
 
 // Create an instance of auth.js.
 let myApsAuth = new MyAuth(clientId, clientSecret);
@@ -21,42 +20,23 @@ let myApsApp = new MyApp(
   accessToken
 );
 
-if (useBetaAPI) {
-  let info = await myApsApp.getModelHierarchyBeta(
-    hubName,
-    projectName,
-    componentName
-  );
+let info = await myApsApp.getModelHierarchy(
+	hubName,
+	projectName,
+	componentName
+);
 
-  if (info) {
-    console.log("Model hierarchy:");
-    printInfo(info.allModelOccurrences, info, "");
-  } 
+if (info) {
+	console.log("Model hierarchy:");
+	printInfo(info.allModelOccurrences, info, "");
+} 
 
-  function printInfo (componentVersions, componentVersion, indent) {
-    console.log(indent + componentVersion.name);
-    let subOccurrences = componentVersions.results.filter(
-      item => item.parentComponentVersion.id === componentVersion.id);
-    for (let occurrence of subOccurrences) {
-      printInfo(componentVersions, occurrence.componentVersion, indent + "  ");
-    }
-  }
-} else {
-  let info = await myApsApp.getModelHierarchy(
-    hubName,
-    projectName,
-    componentName
-  );
-  
-  if (info) {
-    console.log("Model hierarchy:");
-    printInfo(info.componentVersions, info.rootId, "");
-  } 
-  
-  function printInfo (componentVersions, componentVersionId, indent) {
-    console.log(indent + componentVersions[componentVersionId].name);
-    for (let occurrence of componentVersions[componentVersionId].modelOccurrences.results) {
-      printInfo(componentVersions, occurrence.componentVersion.id, indent + "  ");
-    }
+function printInfo (componentVersions, componentVersion, indent) {
+  console.log(indent + componentVersion.name);
+  let subOccurrences = componentVersions.results.filter(
+    item => item.parentComponentVersion.id === componentVersion.id);
+  for (let occurrence of subOccurrences) {
+    printInfo(componentVersions, occurrence.componentVersion, indent + "  ");
   }
 }
+
